e1914bea92e40e698b231da80ba1246bf2f70659
Garfield128 <33193920+Garfield128@users.noreply.github.com>
2017-12-02 17:22:19 +0700

    add exercise 2 Calculator

---
+++ Ex2_Calculator.js
@@ -0,0 +1,22 @@
+var x,y;
+
+function addition(x, y) {
+  return (x + y);
+}
+
+function subtraction(x, y) {
+  return (x-y);
+}
+
+function multiplication(x, y) {
+  return x * y;
+}
+
+function division(x, y) {
+  return x/y;
+}
+
+console.log('Total: ' + addition(5,6));
+console.log('Subtraction: ' + subtraction(5,6));
+console.log('Multiplication: ' + multiplication (5,6));
+console.log('Division: ' + division(5,6));
